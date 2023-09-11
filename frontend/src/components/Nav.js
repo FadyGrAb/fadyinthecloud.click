83d496cbaec7efa7052498a9ba4e61e2be414949
Fady <fady.michel@outlook.com>
2023-09-11 21:23:35 +0300

    nav and sections are responsive

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,14 +3,14 @@ import "../styles/Nav.css";
 const Nav = (props) => {
   const links = props.sections.map((section, key) => {
     return (
-      <a key={key} className="nav-a nav-active" href={`#${section.class}`}>
+      <a key={key} className="nav-item nav-a" href={`#${section.class}`}>
         {section.name}
       </a>
     );
   });
   return (
-    <nav>
-      <a className="nav-brand" href="/">
+    <nav id="nav-bar">
+      <a className="nav-item nav-brand" href="/">
         FadyInTheCloud
       </a>
       {links}
