afc314fa6654b0dcbc24f75486667765a0de2842
Fady <fady.michel@outlook.com>
2023-09-25 12:24:53 +0300

    testing download

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,7 +3,7 @@ import "../styles/Nav.css";
 const Nav = (props) => {
   return (
     <nav id="nav-bar">
-      <a className="nav-item nav-brand" href="/">
+      <a className="nav-item nav-brand" href="#intro">
         <span style={{ color: "red" }}>Y</span>et
         <span style={{ color: "blue" }}>&nbsp;A</span>nother
         <span style={{ color: "orange" }}>&nbsp;D</span>ata
@@ -18,9 +18,15 @@ const Nav = (props) => {
       <a className="nav-item nav-a" href="#lets-connect">
         Let's Connect
       </a>
+      <a className="nav-item nav-a" href="../files/FadyAbdelmassih-resume.pdf">
+        Download My resume
+      </a>
       <a
         className="nav-item nav-a nav-direct-link"
         href="https://fadyinthecloud.click/data-analytics-made-easy"
+        download="fadyAbdelmassih-resume"
+        target="_blank"
+        rel="noreferrer"
       >
         Data Analytics Made Easy Series!
       </a>
--- frontend/src/content/AboutMe.js
+++ frontend/src/content/AboutMe.js
@@ -2,6 +2,10 @@ const AboutMe = () => {
   return (
     <div>
       <h2>About Me</h2>
+      <p>
+        My name is Fady and I come from Egypt 🇪🇬. I'm a telecommunications
+        professional since 2009
+      </p>
     </div>
   );
 };
